03bae9f96766ee8b0146787b9e0e9af83636be28
Marcin <mbuchcik@wp.pl>
2017-05-15 15:45:07 +0200

    Trapezium

--- kolokwium/Trapezium.cs
+++ kolokwium/Trapezium.cs
@@ -9,5 +9,28 @@ namespace kolokwium
     class Trapezium
     {
         public List<Point> vertices = new List<Point>();
+
+        public Trapezium(int a, int b, int c)
+        {
+
+            vertices = new List<Point> {
+                new Point(0, 0),
+                new Point(a, 0),
+                new Point(a, b),
+                new Point(0, c)
+            };
+        }
+
+        public Trapezium(Point pnt, int a, int b, int c)
+        {
+
+            vertices = new List<Point> {
+                pnt,
+                new Point(pnt.x + a, pnt.y),
+                new Point(pnt.x + a, pnt.y + b),
+                new Point(pnt.x, pnt.y + c)
+            };
+        }
+
     }
 }
