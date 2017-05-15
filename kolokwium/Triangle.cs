96e6c88b51f07a70a1b83b4a0f42cb239e599da8
Marcin <mbuchcik@wp.pl>
2017-05-15 15:37:03 +0200

    Triangle

--- kolokwium/Triangle.cs
+++ kolokwium/Triangle.cs
@@ -9,5 +9,28 @@ namespace kolokwium
     class Triangle
     {
         public List<Point> vertices = new List<Point>();
+
+
+        public Triangle(int a, int b)
+        {
+
+            vertices = new List<Point>
+            {
+                new Point(0, 0),
+                new Point(a, 0),
+                new Point(a, (0.5 * b))
+            };
+        }
+
+        public Triangle(Point pnt, int a, int b)
+        {
+
+            vertices = new List<Point>
+            {
+                pnt,
+                new Point(pnt.x + a, pnt.y),
+                new Point(pnt.x + a, pnt.y + (0.5 * b))
+            };
+        }
     }
 }
