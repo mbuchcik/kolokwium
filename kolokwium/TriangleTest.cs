2c08bfe157c077ede53d9de12a85a857f4d43640
Marcin <mbuchcik@wp.pl>
2017-05-15 15:32:47 +0200

    TriangleTest1

--- kolokwium/TriangleTest.cs
+++ kolokwium/TriangleTest.cs
@@ -8,6 +8,26 @@ namespace kolokwium
 {
     class TriangleTest
     {
+        public void Test1()
+        {
+            List<Point> expectedVerticles = new List<Point>
+            {
+            new Point(0, 0),
+            new Point(9, 2),
+            new Point(8, 7)
+        };
 
+            Triangle triangle = new Triangle(9, 7);
+
+            foreach (Point pos in triangle.vertices)
+            {
+                if (!expectedVerticles.Contains(pos))
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+
+        
     }
 }
