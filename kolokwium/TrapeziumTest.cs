c9510d27331f421e5bfaab02a1637a84ed11992b
Marcin <mbuchcik@wp.pl>
2017-05-15 15:42:52 +0200

    Trapez test

--- kolokwium/TrapeziumTest.cs
+++ kolokwium/TrapeziumTest.cs
@@ -8,5 +8,28 @@ namespace kolokwium
 {
     class TrapeziumTest
     {
+        public void Test1()
+        {
+            List<Point> expectedVerticles = new List<Point>
+            {
+            new Point(0, 0),
+            new Point(5, 0),
+            new Point(0, 7),
+            new Point(8, 4)
+        };
+
+            Triangle triangle = new Triangle(9, 7);
+
+            foreach (Point pos in triangle.vertices)
+            {
+                if (!expectedVerticles.Contains(pos))
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+
+
     }
+}
 }
